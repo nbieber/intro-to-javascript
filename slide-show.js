a10cab6cd062b953e29ece799883c159c090333c
Nicole Bieber <nbieber@alum.mit.edu>
2013-08-15 08:39:25 -0400

    replace mootools with jquery

--- slide-show.js
+++ slide-show.js
@@ -1,6 +1,6 @@
 // This first line ensures the document has loaded enough content where it's
 // safe to start meddling with things
-window.addEvent('domready', function() {
+document.ready = function() {
   
   // How wide each image is
   var width = 991;
@@ -9,22 +9,24 @@ window.addEvent('domready', function() {
   var n = 0;
   
   // Number of total slides
-  var count = $$('#slides img').length;
+  var count = $('#slides img').length;
   
   // Dynamically resize the inner DIV element
-  $('inner').setStyle('width', width * count);
+  $('#inner').css('width', width * count);
   
   // On click, advance to the next slide
-  $('slides').addEvent('click', function() {
+  $('#slides').click( function() {
     
+    $($('#slides img')[n]).hide();
+
     // Increment the position by one
     // The modulo operator (%) resets n back to 0 if it exceeds count
     n = (n + 1) % count;
     
     // Slide the view to the correct position
-    $('inner').tween('left', n * -width);
+    $($('#slides img')[n]).show();
     
   }); // click event
 
-}); // domready event
+}; // domready event
 
