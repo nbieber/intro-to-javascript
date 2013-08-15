63c6ab330d5474c27522e1c8393e6bacf56dfbf8
Nicole Bieber <nbieber@alum.mit.edu>
2013-08-15 13:48:20 -0400

    mad-libs

---
+++ mad-libs.js
@@ -0,0 +1,13 @@
+// Create a function startMadLibs
+
+function startMadLibs() {
+
+	// For each span in the document
+	for(             ) {
+		// Get the class of that span
+
+		// Ask the user for a word of that type
+
+		// Replace the contents of the span
+	}
+}
